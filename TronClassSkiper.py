38cebfb37de9e224356abac57cccb5c21d554b77
TW-RF54732 <127068503+TW-RF54732@users.noreply.github.com>
2025-10-09 02:42:14 +0800

    classScanner

--- TronClassSkiper.py
+++ TronClassSkiper.py
@@ -5,25 +5,19 @@ from alive_progress import alive_bar
 import mySession
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-classCode = 00000
-url = f"https://eclass.yuntech.edu.tw/api/course/activities-read/{classCode}"
-
 session_id = mySession.mySession #我把我的session藏在mySession，自己要用的話把import拿掉直接用字串輸入你的session!!!!
-
-targetTime = 100
-jumpScale = 100
-current = 1
-
 session = requests.Session()
 session.cookies.set("session", session_id)
+jumpScale = 120
 
-def sendPost(start, end):
+def sendPost(url,start, end):
     payload = {"start": start, "end": end}
     response = session.post(url, json=payload, verify=False)  # ← 改這裡！
     # print(response.status_code)
     # print(response.text)
 
 def getVideoTime(id):
+    classCode = id
     aurl = f"https://eclass.yuntech.edu.tw/api/activities/{classCode}?sub_course_id=0"
     response = session.get(aurl,verify=False)  # ← 改這裡！
     data = response.json()
@@ -31,7 +25,23 @@ def getVideoTime(id):
     # 取得第一個 upload 中第一個影片的 duration
     duration = data["uploads"][0]["videos"][0]["duration"]
     return int(duration)
-    
+
+def API_Skip(classCode):
+    url = f"https://eclass.yuntech.edu.tw/api/course/activities-read/{classCode}"
+    targetTime = getVideoTime(classCode)
+    current = 0
+    while current < targetTime:
+        time.sleep(0.2)
+        if current + jumpScale >= targetTime:
+            sendPost(url, current, targetTime)
+            yield targetTime - current  # 回傳「增加了多少」
+            current = targetTime
+        else:
+            sendPost(url, current, current + jumpScale)
+            yield jumpScale  # 每次 yield「增加 jumpScale」
+            current += jumpScale
+
+
 if __name__ == "__main__":
     classCode = input("輸入課程碼:")
     url = f"https://eclass.yuntech.edu.tw/api/course/activities-read/{classCode}"
--- classScanner.py
+++ classScanner.py
@@ -1,8 +1,32 @@
 import requests
+import urllib3
 from mySession import mySession
+from alive_progress import alive_bar
+import TronClassSkiper
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-session_id = mySession
 session = requests.Session()
-session.cookies.set("session", session_id)
+session.cookies.set("session", mySession)
 
-session.get()
+url = "https://eclass.yuntech.edu.tw/api/courses/116976/activities?sub_course_id=0"
+
+response = session.get(url, verify=False)
+data = response.json()
+
+# 取出 activities 陣列
+activities = data.get("activities", [])
+
+# 找出 completion_criterion_key == "none" 的 id
+target_ids = [
+    a["id"]
+    for a in activities
+    if a.get("type") == "online_video"
+]
+length = 0
+for id in target_ids:
+    length +=TronClassSkiper.getVideoTime(id)
+
+with alive_bar(length,title="爆破大隊爆破中") as bar:
+    for id in target_ids:
+        for progress in TronClassSkiper.API_Skip(id):
+            bar(progress)
